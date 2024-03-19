d05e07fdf5ceffe6d015ff4ff5e40ea6aa0f9db6
DeflateAwning <11021263+DeflateAwning@users.noreply.github.com>
2024-03-18 18:07:14 -0600

    Convert to preferred ADC driver implementation

--- ADC_Driver_Project/adc.c
+++ ADC_Driver_Project/adc.c
@@ -10,25 +10,49 @@
 #include "delay.h"
 
 void init_adc(void) {
-    // Configure ADC module
-    AD1PCFG = 0xFFFE; // AN5 enabled as analog input (RA3/AN5)
-    AD1CON1 = 0x00E0; // ADC Module off, Manual Sample, auto-conversion
-    AD1CHS = 0x0005;  // Connect AN5 as S/H+ input
-    AD1CSSL = 0;      // No scanning required
-    AD1CON3 = 0x1F02; // Tad = 128*Tcy, Tad = 8*Tcy
-    AD1CON2 = 0;      // No interrupts, AVdd and AVss as reference voltages
-    
-    // Turn on ADC module
     AD1CON1bits.ADON = 1; // Enable ADC module
+    
+    AD1CON1bits.SSRC = 0b111;
+    
+    AD1CON1bits.ASAM = 0;
+    
+    AD1CON2bits.VCFG = 0b000; // voltage range VDD to VSS
+    
+    AD1CON2bits.CSCNA = 0; // do not scan inputs
+    
+    AD1CON2bits.BUFM = 0; // buffer holds a single 16-bit value
+    
+    AD1CON2bits.ALTS = 0; // always use Mux A
+    
+    // Configure the ADC’s sample time by setting bits in AD1CON3 shown in slide 17
+    AD1CON3bits.ADRC = 0; // system clock
+    
+    AD1CON3bits.SAMC = 0b11111;
+    AD1CON3bits.ADCS = 0b11111;
+    
+    // General note: Ensure sample time is 1/10th of signal being sampled or as per application’s speed and needs
+    
+    AD1CHSbits.CH0NA = 0;
+    AD1CHSbits.CH0SA = 0b0101; // select AN5 channel
+    
+    AD1PCFGbits.PCFG5 = 0; // analog mode, ADC port disabled
+    
+    AD1CSSLbits.CSSL5 = 0; // analog channel omitted from input scan
+    
 }
 
 uint16_t read_adc_value(void) {
+    // Enable ADC module
+    AD1CON1bits.ADON = 1;
+    
+    
     AD1CON1bits.SAMP = 1; // Start sampling
-//    __delay_us(10); // Sampling time (depends on the specific requirements)
-    delay32_us(1000);
-    
-    AD1CON1bits.SAMP = 0; // End sampling, start conversion
-    
     while (!AD1CON1bits.DONE); // Wait for conversion to complete
-    return ADC1BUF0; // Return the ADC conversion result
+    
+    const uint16_t adc_value = ADC1BUF0;
+    
+    AD1CON1bits.SAMP = 0; // End sampling
+    AD1CON1bits.ADON = 0; // Turn off ADC
+    
+    return adc_value;
 }
